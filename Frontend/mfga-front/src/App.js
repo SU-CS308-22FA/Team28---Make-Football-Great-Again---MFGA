2d4acac22b2f30111b2346fa55bcdf310328b697
alivehbi <alivehbi@sabanciuniv.edu>
2022-11-01 09:04:40 +0300

    app.js changed

--- Frontend/mfga-front/src/App.js
+++ Frontend/mfga-front/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import { Register } from './components/Register/register';
+import { Delete } from "./components/Profile/delete";
 
 function App() {
   return (
     <div className="App">
-      <Register/>
+      <Delete/>
     </div>
   );
 }
