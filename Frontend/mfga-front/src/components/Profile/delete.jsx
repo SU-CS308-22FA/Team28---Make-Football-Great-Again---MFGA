643fa6d6518ed4a610138af3818b083bbe4721d6
alivehbi <alivehbi@sabanciuniv.edu>
2022-11-01 08:16:54 +0300

    Frontend
    Frontend updated

--- Frontend/mfga-front/src/components/Profile/delete.jsx
+++ Frontend/mfga-front/src/components/Profile/delete.jsx
@@ -1,2 +1,45 @@
-import React from "react";
+import React,{useState} from "react";
 import axios from "axios";
+
+const Delete=()=>{
+    const[values,setValues] = useState({
+        username:"",
+    });
+};
+
+const handleChange=(e)=>{
+    const{name,value}=e.target;
+    setValues({
+        ...values,
+        [name]:value,
+    });
+};
+
+const handleSubmit = (e)=>{
+    e.preventDefault();
+    const {name,value}=e.target;
+    const deleted={
+        ...values,
+        [name]:value,
+    };
+
+
+
+
+    axios.delete("http://localhost:4000/delete")
+    .then((res)=>{
+        if(res.status==200){
+            console.log("Deleted");
+        }
+        else{
+            console.log("Error happened, cannot delete!");
+        }
+    })
+    .catch((err)=>{
+        console.log(err);
+    });
+
+    setValues({
+        username:"",
+    });
+}
