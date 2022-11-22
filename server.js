51f7dca62824aed2d57350058af51c345e0145d1
alperkaan01 <88709260+alperkaan01@users.noreply.github.com>
2022-11-22 18:31:28 +0300

    cors origin changed

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(cookieParser())
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors({
-  origin: "https://mfga.herokuapp.com",
+  origin: "*",
   credentials: true,
 }));
 
