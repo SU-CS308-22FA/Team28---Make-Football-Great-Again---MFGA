d076c2642b1a9e3a5d48416f09d617a9deb9c3a0
Emirhan Kaya <aemirhan@sabanciuniv.edu>
2023-01-04 21:17:51 +0300

    Route name change
    Route changed for fixedbugs

--- client/src/Pages/FixedBugs/fixedBugs.js
+++ client/src/Pages/FixedBugs/fixedBugs.js
@@ -10,7 +10,7 @@ export const FixedBugs = ()=>{
 
     useEffect(() => {
         axios
-            .get("http://localhost:4000/fixedBugs")
+            .get("http://localhost:4000/fixBug")
             .then( (res) => {
               const bugs = res.data;
               setBugs(bugs);
